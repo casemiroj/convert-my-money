d08e4c1aee4e66c2bb0e7506cae8a3a2bd00cf5e
casemiro <jv.casemiro@hotmail.com.br>
2021-05-12 16:23:47 -0300

    Add Header e Footer

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require('path')
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render('home')
