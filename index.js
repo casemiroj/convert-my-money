681e1fc2bf3924d191c04bc7bc1cd3d543c9f31c
casemiro <jv.casemiro@hotmail.com.br>
2021-05-24 10:12:52 -0300

    Pegando cotação do BC

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express()
 const path = require('path')
 
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api-bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async(req, res) => {
+  const cotacao = await apiBCB.getCotacao()
+  res.render('home', {
+    cotacao
+  })
 })
 
 app.get('/cotacao', (req, res) => {
